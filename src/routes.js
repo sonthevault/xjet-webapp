50fd10f689912aab4cc2f2577beb08ce2c5f984d
Son Pham <pxson.2903@gmail.com>
2019-11-10 22:42:06 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import Tables from "./views/pages/tables/Tables.jsx";
 import AccountSetting from "./containers/AccountSetting";
+import OrderPage from "./containers/OrderPage";
 
 const routes = [
   {
-    path: "/settings",
-    name: "Account Settings",
+    path: "/profile",
+    name: "User Profile",
     icon: "fa fa-cog text-red",
     component: AccountSetting,
-    layout: "/admin"
+    layout: "/user"
+  },
+  {
+    path: "/token",
+    name: "Token",
+    icon: "fa fa-cog text-red",
+    component: OrderPage,
+    layout: "/user"
   },
 ];
 
