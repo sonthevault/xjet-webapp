0558de4f4a9968fad21124a4adecead632e8fe7d
Son Pham <pxson.2903@gmail.com>
2019-11-10 22:51:48 +0700

    Add support page

---
+++ src/containers/SupportPage/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { withNamespaces } from "react-i18next";
+import { compose } from "recompose";
+
+class OrderPageContainer extends Component {
+  state = {};
+
+  openInNewTab  = (url) => {
+    var win = window.open(url, '_blank');
+    win.focus();
+  }
+
+  componentDidMount() {
+    this.openInNewTab("https://xjet.zendesk.com/");
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+    return null
+  }
+}
+
+export default compose(
+  withNamespaces("order", { wait: true }),
+  withRouter,
+)(OrderPageContainer);
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Tables from "./views/pages/tables/Tables.jsx";
 import AccountSetting from "./containers/AccountSetting";
 import OrderPage from "./containers/OrderPage";
+import SupportPage from "./containers/SupportPage";
 
 const routes = [
   {
@@ -15,6 +16,13 @@ const routes = [
     name: "Token",
     icon: "fa fa-cog text-red",
     component: OrderPage,
+    layout: "/user"
+  },
+  {
+    path: "/support",
+    name: "Customer Support",
+    icon: "fa fa-cog text-red",
+    component: SupportPage,
     layout: "/user"
   },
 ];
